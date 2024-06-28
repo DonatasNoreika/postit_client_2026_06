208d07dffaeb5f0c209b5412cc3227df5f09e1bc
DonatasNoreika <donoras@gmail.com>
2024-06-28 10:11:19 +0300

    Įrašai atvaizduojami html'uose, paspaudus ant įrašo, matoma detali informacija.

--- api_example_client/postit_client/urls.py
+++ api_example_client/postit_client/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from .views import posts, post
 
 urlpatterns = [
+    path("", posts, name="posts"),
+    path("posts/<int:post_id>", post, name="post"),
 ]
--- api_example_client/postit_client/views.py
+++ api_example_client/postit_client/views.py
@@ -3,4 +3,11 @@ import requests
 
 # Create your views here.
 def posts(request):
-    return render(request)
+    r = requests.get("http://127.0.0.1:6500/posts/")
+    posts_dict = r.json()
+    return render(request, template_name="posts.html", context={"posts": posts_dict})
+
+def post(request, post_id):
+    r = requests.get(f"http://127.0.0.1:6500/posts/{post_id}")
+    post_dict = r.json()
+    return render(request, template_name="post.html", context={"post": post_dict})
