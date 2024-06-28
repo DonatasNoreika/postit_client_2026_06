b8ac635ed2c3882523433bb53e7fb025e27be793
DonatasNoreika <donoras@gmail.com>
2024-06-28 09:30:23 +0300

    Initial commit

---
+++ api_example_client/postit_client/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PostitClientConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'postit_client'
---
+++ api_example_client/postit_client/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+def posts(request):
+    return render(request)
