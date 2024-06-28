431ec5fd0536b18b18d6c7bfc3ef4c3b0c52f4b5
DonatasNoreika <donoras@gmail.com>
2024-06-28 11:35:36 +0300

    Leidžiama sukurti naują įrašą (su hardcode token'u)

--- api_example_client/postit_client/urls.py
+++ api_example_client/postit_client/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import posts, post
+from .views import posts, post, post_create
 
 urlpatterns = [
     path("", posts, name="posts"),
     path("posts/<int:post_id>", post, name="post"),
+    path("posts/new", post_create, name="post_create"),
 ]
--- api_example_client/postit_client/views.py
+++ api_example_client/postit_client/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 import requests
 
+token = "Token 1961a9e2cec10da70006b7ac51086a788ebb8882"
 # Create your views here.
 def posts(request):
     r = requests.get("http://127.0.0.1:6500/posts/")
@@ -11,3 +13,18 @@ def post(request, post_id):
     r = requests.get(f"http://127.0.0.1:6500/posts/{post_id}")
     post_dict = r.json()
     return render(request, template_name="post.html", context={"post": post_dict})
+
+# @login_required
+def post_create(request):
+    if request.method == "POST":
+        data = {
+            "title": request.POST['title'],
+            "body": request.POST['body'],
+        }
+        headers = {'Authorization': token}
+        r = requests.post("http://127.0.0.1:6500/posts/", data=data, headers=headers)
+        print(r.json())
+        return redirect('posts')
+
+    if request.method == "GET":
+        return render(request, "post_create.html")
